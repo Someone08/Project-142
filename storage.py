89622f2b91d47b9fb8c64aedb3078439ce32034f
Someone08 <83487451+Someone08@users.noreply.github.com>
2024-03-03 18:03:22 -0800

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,39 +1,11 @@
-from flask import Flask, jsonify, request
-import pandas as pd
-import csv 
+import csv
 
-app = Flask(__name__)
+all_articles = []
 
-articles = pd.read_csv('articles.csv')
+with open("articles.csv") as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_articles = data[1:]
 
-articles.columns = ['id'] + articles.columns[1:].tolist()
-
-all_articles = articles.values.tolist()
 liked_articles = []
 not_liked_articles = []
-
-@app.route('/get_article', methods=['GET'])
-def get_article():
-    if all_articles:
-        return jsonify(all_articles[0])
-    else:
-        return jsonify({"message": "No more articles to show."})
-
-@app.route('/like_article', methods=['POST'])
-def like_article():
-    if all_articles:
-        liked_articles.append(all_articles.pop(0))
-        return jsonify({"message": "Article liked successfully."})
-    else:
-        return jsonify({"message": "No more articles to like."})
-
-@app.route('/dislike_article', methods=['POST'])
-def dislike_article():
-    if all_articles:
-        not_liked_articles.append(all_articles.pop(0))
-        return jsonify({"message": "Article disliked successfully."})
-    else:
-        return jsonify({"message": "No more articles to dislike."})
-
-if __name__ == '__main__':
-    app.run(debug=True)
